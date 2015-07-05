3c6834cdb128c532dad5b63668251a6573ee8fd9
Damien Waits <dsw0002@gmail.com>
2015-07-05 11:37:19 -0500

    Update ContamScreen.sh

--- ContamScreen.sh
+++ ContamScreen.sh
@@ -1,4 +1,18 @@
-#This program screens a given assembly or assembly for contamination based on two #reference databases supplied by the user as fasta files, parameter 1 and parameter 2. #Parameter 1 should include sequences that are similar to those of the targeted assembly. #Parameter 2 should include sequences of known or suspected contaminants. These files #are merged and used to generate a BLAST database which is run on the target assembly. #The best hit for each contig in the assembly is chosen based on e-value score comparison. #E-values are compared based on order of magnitude and the sensitivity of this comparison i#s decided by the user. Parameter 3 should be an integer and corresponds to how much #better a “contam” hit must be than the best “good” hit to be considered contaminated. #Example: If parameter 3 is input as 5 and the best “good” hit for Contig1 is 1e-10 and the #best “contam” hit is 1e-16, Contig1 is returned as “contaminated”. If the best “contam” #hit were 1e-13, Contig1 would be returned as “suspect” for manual evaluation. Parameter #4 is similar except it is how much better a “good” hit must be than a “contam” hit. Three #files are generated as output: TAXON-good.fasta, TAXON-contam.fasta, TAXON-#suspect.fasta. BLAST output files can be found in the misc_intermediate_files directory.
+#This program screens a given assembly or assembly for contamination based on two 
+#reference databases supplied by the user as fasta files, parameter 1 and parameter 2. 
+#Parameter 1 should include sequences that are similar to those of the targeted assembly. 
+#Parameter 2 should include sequences of known or suspected contaminants. These files 
+#are merged and used to generate a BLAST database which is run on the target assembly. 
+#The best hit for each contig in the assembly is chosen based on e-value score comparison. 
+#E-values are compared based on order of magnitude and the sensitivity of this comparison 
+#is decided by the user. Parameter 3 should be an integer and corresponds to how much 
+#better a “contam” hit must be than the best “good” hit to be considered contaminated. 
+#Example: If parameter 3 is input as 5 and the best “good” hit for Contig1 is 1e-10 and the 
+#best “contam” hit is 1e-16, Contig1 is returned as “contaminated”. If the best “contam” 
+#hit were 1e-13, Contig1 would be returned as “suspect” for manual evaluation. Parameter 
+#4 is similar except it is how much better a “good” hit must be than a “contam” hit. Three 
+#files are generated as output: TAXON-good.fasta, TAXON-contam.fasta, TAXON-
+#suspect.fasta. BLAST output files can be found in the misc_intermediate_files directory.
 
 #This program was developed by Damien S Waits and Kevin M Kocot. Version July 3rd, #2015
 
@@ -10,20 +24,21 @@ mkdir "misc_intermediate_files"
 sed -i 's/^>/>good\ /' $1 > good.fasta
 sed -i 's/^>/>contam\ /' $2 > contaminated.fasta
 
-#Writes both good.fasta and contaminated.fasta to an all.fasta #and generates a blast database since e-values are not #informative across multiple blast searches.
+#Writes both good.fasta and contaminated.fasta to an all.fasta and generates a blast database since e-values are not 
+#informative across multiple blast searches.
 cat good.fasta contaminated.fasta > all.fasta
 makeblastdb -in all.fasta -dbtype nucl -title ALL -out ALL
 
-#Performs the following actions on all files that have the #suffix .fa until “done” is read.
+#Performs the following actions on all files that have the suffix .fa until “done” is read.
 for FILENAME in *.fa
 do
 
-#Removes line breaks from taxon.fa #using nentferner.pl distributed with HaMStR
+#Removes line breaks from taxon.fa using nentferner.pl distributed with HaMStR
 nentferner.pl -in=$FILENAME -out=$FILENAME".nent" 
 rm $FILENAME
 mv $FILENAME".nent" $FILENAME
 
-#Removes unneeded header text that will make blast output #unnecessarily long.
+#Removes unneeded header text that will make blast output unnecessarily long.
 sed -i '/^>/ s/ .\+//g' $FILENAME
 sed -i '/^>/ s/;.\+//g' $FILENAME
 
@@ -36,10 +51,11 @@ rm -rf $FILENAME
 #Restores original file.
 mv $FILENAME".tmp" $FILENAME
 
-#Sets variable "taxon" equal to the part of the file name before #the extension.
+#Sets variable "taxon" equal to the part of the file name before the extension.
 taxon=`echo $FILENAME | cut --delimiter=. --fields=1` 
 
-#Performs blast search on assembly using the ALL database that #includes both good and contam sequences, and formats output to #a table. 
+#Performs blast search on assembly using the ALL database that includes both good and contam sequences,
+#and formats output to a table. 
 blastn -db ALL -query $FILENAME -num_descriptions 10 -num_alignments 10 -num_threads 6 > $taxon"_vs_all.txt" 
 blast2table2.pl -format 10 -evalue 0.0001 $taxon"_vs_all.txt" > $taxon"_vs_all.table"
 
@@ -47,14 +63,16 @@ blast2table2.pl -format 10 -evalue 0.0001 $taxon"_vs_all.txt" > $taxon"_vs_all.t
 sed 's/[1-9]e-0*//' $taxon"_vs_all.table" > temp.table
 sed -i 's/0.0/0/' temp.table
 
-#Loops through all lines in the table format of the blast #output.
+#Loops through all lines in the table format of the blast output.
 while read line;
 do
 
 #Returns the sixth column of the table.
 thisLine=`echo $line | awk '{print $6}'`
 
-#The following nested if statements compares the e-values of #hits to the same contig in the targeted assembly. Contaminant #or true sequences are determined based on the e-value of the #hit. If the hit is within a threshold determined by the end #user, it is instead output to a file of suspect sequences.
+#The following nested if statements compares the e-values of hits to the same contig in the targeted assembly.
+#Contaminant or true sequences are determined based on the e-value of the hit. If the hit is within a 
+#threshold determined by the end user, it is instead output to a file of suspect sequences.
 if [ "$lastLine" == "" ]
 then
     lastLine=$thisLine
@@ -126,7 +144,8 @@ fi
     
 done <  $taxon"_vs_all.table"
 
-#Writes out the name of each contig to either good or contam #headers files depending on the comparisons from the above if #statement.
+#Writes out the name of each contig to either good or contam headers files depending on the 
+#comparisons from the above if statement.
 if [ "$perfect" != "" ]
 then        
         echo "$perfectLine" >> $perfect"_headers.txt"
@@ -146,7 +165,7 @@ sort good_headers.txt | uniq > good_headers_sorted.txt
 sort suspect_headers.txt | uniq > suspect_headers_sorted.txt
 sort contam_headers.txt | uniq > contam_headers_sorted.txt
 
-#Extracts the sequences that belong to the headers in the above #files.
+#Extracts the sequences that belong to the headers in the above files.
 select_contigs.pl -n good_headers_sorted.txt $FILENAME
 $taxon"-good.fasta"
 select_contigs.pl -n contam_headers_sorted.txt $FILENAME $taxon"-contam.fasta"
